da41f9d02d4abb118d7b70edeccd691bb76ee9d9
이호진 <gommind@naver.com>
2023-11-29 19:39:39 +0900

    feat #1 kafka-streams-test init

---
+++ src/main/java/com/example/kafkastreamtest/KafkaStreamTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkastreamtest/KafkaStreamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
